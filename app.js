e54d7483d04b4ef8c174337bd830c8e6e2fd51ac
Md. Tanvir Parvej Badhon <69302248+imtanvir@users.noreply.github.com>
2022-03-07 14:38:27 +0600

    Fix all error and Learn a lot from logical part seriously.

--- app.js
+++ app.js
@@ -1,90 +1,147 @@
-
+// Search Button click and action happend code
 document.getElementById('search-btn').addEventListener('click', getData => {
-    const search_input = document.getElementById('search-input-text');
-    fetch(`https://openapi.programming-hero.com/api/phones?search=${search_input.value}`)
-        .then(response => response.json())
-        .then(data => createData(data.data))      
+        const search_input = document.getElementById('search-input-text');
+        fetch(`https://openapi.programming-hero.com/api/phones?search=${search_input.value}`)
+            .then(response => response.json())
+        .then(data => { createData(data.data), falseChecker(data.status) })
+    // Search keyword true or false check 
+        const falseChecker = isFlase => {
+            if (isFlase === false) {
+                const errDiv = document.createElement('div');
+                errDiv.classList.add('w-100','bg-danger','mt-5');
+                errDiv.innerHTML = `
+                <h1 class="text-white mt-5 fw-bold text-center">Your Search Keyword do not match with any items!</h1>`;
+                document.getElementById('rowForMobileItemsShow').appendChild(errDiv);
+            }
+    }
+    // Previous search result clear code 
+        document.getElementById('rowForMobileItemsShow').innerHTML = '';
 });
-
+// Start Dom creation and search result show code
 const createData = dataperameter => {
-    // console.log(dataperameter.data[0]);
-    dataperameter.forEach(phone => {
-        const row_div = document.getElementById('rowid');
+    showItemNumStart = 0;
+    showItemNumEnd = 20;
+    showItemClassName = '';
+// If Search result more then 20, than prevent to show the more result code 
+    if (dataperameter.length <= 20) {
+        showItemNumEnd = 20;
+        document.getElementById('show-more-btn').setAttribute("class", "d-none btn btn-primary");
+    } else if (dataperameter.length > 20) {
+        showItemNumEnd = 20;
+        document.getElementById('show-more-btn').removeAttribute("class", "d-none btn btn-primary");
+    }
+    // Matched keyword result items creation code 
+    dataperameter.slice(showItemNumStart,showItemNumEnd).forEach(phone => {
+        fetch(`https://openapi.programming-hero.com/api/phone/${phone.slug}`)
+            .then(response => response.json())
+            .then(dataIs => slugData(dataIs.data))
+            
+        const row_div = document.getElementById('rowForMobileItemsShow');
         const createDataDiv = document.createElement('div');
         // code for details data start
-        const phone_details_url = `https://openapi.programming-hero.com/api/phone/${phone.slug}`;
-        fetch(phone_details_url)
-        .then(response => response.json())
-            .then(detailsData => detailsGetdata(detailsData))
-        const detailsGetdata = detailData => {
-            dataSet = detailData.mainFeatures;
-        }
         createDataDiv.classList.add('col');
-        createDataDiv.innerHTML =`
-        <div class="card">
-            <img src="${phone.image}" style="width: 60%;" class="card-img-top mx-auto pt-3" alt="phone_photo">
-            <div class="card-body">
-                <h5 class="card-title">Name: ${phone.phone_name}</h5>
-                <h6 class="card-title">Brand: ${phone.brand}</h6>
-                <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#detailsbtn">Details</button>
-            </div>
-        </div>
-
-<!-- Details model popup -->
-<div class="modal fade" id="detailsbtn" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-<div class="modal-content">
-<div class="modal-header">
-<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-</div>
-<div class="modal-body">
-<div class="row row-cols-1 row-cols-md-1 g-4">
-      <div class="col col-md">
-        <div class="card" style="border:none;">
-              <img src="apple-iphone-13-pro-max.jpg" style="width: 60%; height: 50%;" class="card-img-top" alt="...">
-        </div>
-      </div>
-      <div class="col col-md">
-      <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Release Date:</h5>
-                <div>
-                    <h6 class="card-title">Features:</h6>
-                    <ul class="list-group" id="features_details">
-                        <!-- foreEach loop  -->
-                        <li class="list-group-item"></li>
-                    </ul>
-                    
-                    <h6 class="card-title">Sensors:</h6>
-                    <ul class="list-group">
-                        <!-- foreEach loop  -->
-                        <li class="list-group-item"></li>
-                    </ul>
-                    
-                    <h6 class="card-title">Others:</h6>
-                    <ul class="list-group">
-                            <!-- foreEach loop  -->
-                            <li class="list-group-item"></li>
-                    </ul>
+        const slugData = slugDataIs => {
+            let releaseDateIs = '';
+            if (slugDataIs.releaseDate != '') {
+                releaseDateIs = slugDataIs.releaseDate;
+            } else {
+                releaseDateIs = "Not included.";
+            }
+            createDataDiv.innerHTML = `
+            <div class="card cardDetail">
+                <img src="${phone.image}" style="width: 60%;" class="card-img-top mx-auto pt-3" alt="${phone.slug}">
+                <div class="card-body">
+                    <h5 class="card-title">Name: ${phone.phone_name}</h5>
+                    <h6 class="card-title">Brand: ${phone.brand}</h6>
+                    <button id="${phone.slug}" type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="${'#idis' + phone.slug.slice(-5)}">Details</button>
                 </div>
-                <button type="button" class="btn btn-primary">Buy</button>
             </div>
-      </div>
-</div>
-</div>
-<div class="modal-footer">
-<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-</div>
-</div>
-</div>
-</div>
-</div>`;
+            <!-- Details modal popup -->
+            <div class="modal fade" id="${'idis' + phone.slug.slice(-5)}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+            <div class="modal-content">
+            <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">${phone.phone_name}</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <div class="row row-cols-1 row-cols-md-1 g-4">
+            <div class="col col-md">
+                <div class="card" style="border:none;">
+                    <img src="${phone.image}" style="width: 60%; height: 50%;" class="card-img-top details-img mx-auto" alt="${phone.slug}">
+                </div>
+            </div>
+            <div class="col col-md">
+            <div class="card" id="">
+                    <div class="card-body">
+                    <h5 class="card-title fw-bold" id="${'rlsdate' + phone.slug.slice(-5)}">Release Date: <span class="fw-normal">${slugDataIs.releaseDate}</span></h5>
+                    <div>
+                        <h6 class="card-title fw-bold">Features:</h6>
+                            <ul class="list-group" id="${'mainfeature'+phone.slug.slice(-5)}"></ul>
+                        <h6 class="card-title fw-bold">Others:</h6>
+                            <ul class="list-group" id="${'others'+phone.slug.slice(-5)}"></ul>
+                    </div>
+                    <button type="button" class="btn btn-primary my-3">Buy</button>
+                </div>
+        </div>
+    </div>
+        </div>
+        <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>`;
+        }
         row_div.appendChild(createDataDiv);
-            // for (const [key, value] of Object.entries(dataSet)) {
-            //     const li = createElement('li');
-            //     li.innerHTML = `${key}: ${value}`;
-            //     document.getElementById('features_details').appendChild(li);
-            // }
-    })
+    });
+// Details button click and details show code
+    const onClick = (event) => {
+        if (event.target.nodeName === 'BUTTON' && (event.target.innerText == 'Details')) {
+            if (document.getElementById(`${'mainfeature' + event.target.id.slice(-5)}`).innerText.length == 0) {
+                let slugIs = `https://openapi.programming-hero.com/api/phone/${event.target.id}`;
+                fetch(slugIs)
+                    .then(response => response.json())
+                    .then(data => {
+                        featureDataFunc(data.data);
+                    })
+// Details Validation code
+                const featureDataFunc = dataSet => {
+                    if (dataSet.mainFeatures == undefined || dataSet.mainFeatures == null) {
+                            const li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.innerHTML = `<p style="font-size:14px;" class="h6">Not include yet.</p>`;
+                            document.getElementById(`${'mainfeature' + event.target.id.slice(-5)}`).appendChild(li);
+                    }
+                    if (dataSet.mainFeatures != undefined || dataSet.mainFeatures != null) {
+                        for (const [key, value] of Object.entries(dataSet.mainFeatures)) {
+                            const li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.innerHTML = `<p style="font-size:14px;" class="h6"><span class="fw-bold">${key.toUpperCase()}:</span> ${value}</p>`;
+                            document.getElementById(`${'mainfeature' + event.target.id.slice(-5)}`).appendChild(li);
+                        }
+                    }
+
+                    if (dataSet.others == undefined || dataSet.others == null) {
+                            const li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.innerHTML = `<p style="font-size:14px;" class="h6">Not include yet.</p>`;
+                            document.getElementById(`${'others' + event.target.id.slice(-5)}`).appendChild(li);
+                    }
+                    if (dataSet.others != undefined || dataSet.others != null) {
+                        for (const [key, value] of Object.entries(dataSet.others)) {
+                            const li = document.createElement('li');
+                            li.classList.add('list-group-item');
+                            li.innerHTML = `<p style="font-size:14px;" class="h6"><span class="fw-bold">${key}:</span> ${value}</p>`;
+                            document.getElementById(`${'others' + event.target.id.slice(-5)}`).appendChild(li);
+                        }
+                    }
+                }
+            } else {
+                return;
+            }
+            }
+        }
+        window.addEventListener('click', onClick);
 }
